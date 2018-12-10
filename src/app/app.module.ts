bfe4c9bd47ea409c35ab841e68b408213ab48990
macaulay1001 <macaulayhazza@gmail.com>
2018-12-10 10:15:37 -0200

    comitando teste

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoguinComponent } from './loguin/loguin.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoguinComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/loguin/loguin.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../usuario.service';
+import { Usuario } from '../usuario';
+
+@Component({
+  selector: 'app-loguin',
+  templateUrl: './loguin.component.html',
+  styleUrls: ['./loguin.component.css']
+})
+export class LoguinComponent implements OnInit {
+
+  usuarios: Usuario [];
+  usuarioInput: Usuario;
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+    this.get();
+  }
+
+  get() : void {
+    this.usuarios = this.usuarioService.getUsuario();
+    console.log(this.usuarios);
+  }
+
+  logar(usuarioInput: Usuario){
+    if (usuarioInput.loguin !== '' || usuarioInput.senha !=='') {
+      
+    }
+  }
+
+  autenticaLoguin(loguin : string): boolean{
+    this.usuarios.forEach(element => {
+      if(element.loguin === loguin){
+        return true;
+      }
+    });
+    return true;
+  }
+
+  autenticaSenha(senha : string): boolean{
+    this.usuarios.forEach(element => {
+      if(element.loguin === senha){
+        return true;
+      }
+    });
+    return true;
+  }
+
+}
